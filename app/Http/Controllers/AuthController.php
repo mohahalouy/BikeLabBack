29ac271ec53217069061c5f1a28e245ebf317da1
Moha <mohahalouy@gmail.com>
2022-02-14 11:06:10 +0100

    commit init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Facade\FlareClient\Http\Response;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        return User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response(['message'=>'invalid credentials'], \Symfony\Component\HttpFoundation\Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user=Auth::user();
+
+        $token=$user->createToken('token')->plainTextToken;
+
+        $cookie=cookie('jwt',$token,60*24);
+
+        return response(['message'=>$token])->withCookie($cookie);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $email= $request->input('email');
+        $user = new User();
+
+        $usario = $user->query()->where('email', $email)->first();
+
+        $valor = $user->where('email', $usario->email)->update(
+            [ 'password' => Hash::make($request->input('password'))]
+        );
+
+        if ($valor) {
+            $mensaje = "REGISTRO ACTUALIZADO";
+        } else {
+            $mensaje = "REGISTRO SIN CAMBIOS";
+        }
+
+        return $mensaje;
+    }
+
+    public function user()
+    {
+        return Auth::user();
+    }
+
+    public function logout()
+    {
+        $cookie=Cookie::forget('jwt');
+        return response(['message' => 'success'])->withCookie($cookie);
+    }
+}
